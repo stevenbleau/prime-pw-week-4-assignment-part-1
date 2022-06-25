1fdcd777f792efba7e68817ca73a2ab4c21fffbf
Steven Bleau <bleausd@gmail.com>
2022-06-25 15:55:50 -0500

    stretch goal #8

--- assignment/scripts/function-practice.js
+++ assignment/scripts/function-practice.js
@@ -121,9 +121,19 @@ console.log(find(5,arr3));
 // ----------------------
 // 8. Function to check if a letter is the first letter in a 
 //    string. Return true if it is, and false otherwise
-function isFirstLetter(letter, string) {
 
+
+function isFirstLetter(letter, string) {
+  const a = Array.from(string);
+  console.log(a);
+  if (letter === a[0]){
+    return true;
+  }
+  else{
+    return false;
+  }
 }
+
 console.log( 'isFirstLetter - should say true', isFirstLetter('a', 'apple') );
 console.log( 'isFirstLetter - should say false', isFirstLetter('z', 'apple') );
 
